77d63e412db9f9fc671d02321313175eb20de706
ma. fernanda rodriguez r <mafda13@gmail.com>
2020-05-03 13:17:38 -0300

    include path, and fix bugs

--- blog/mysite/blog/urls.py
+++ blog/mysite/blog/urls.py
@@ -1,11 +1,10 @@
-# from django.conf.urls import url
 from django.urls import path
 from blog import views
 
 urlpatterns = [
 	path('', views.PostListView.as_view(), name='post_list'),
 	path('about/', views.AboutView.as_view(), name='about'),
-	path('post/(?P<pk>\d+)', views.PostDetailView.as_view(), name='post_datail'),
+	path('post/(?P<pk>\d+)', views.PostDetailView.as_view(), name='post_detail'),
 	path('post/new/', views.CreatePostView.as_view(), name='post_new'),
 	path('post/(?P<pk>\d+)/edit/', views.PostUpdateView.as_view(), name='post_edit'),
 	path('post/(?P<pk>\d+)/remove/', views.PostDeleteView.as_view(), name='post_remove'),
@@ -14,5 +13,4 @@ urlpatterns = [
 	path('post/(?P<pk>\d+)/approve/', views.comment_approve, name='comment_approve'),
 	path('post/(?P<pk>\d+)/remove/', views.comment_remove, name='comment_remove'),
 	path('post/(?P<pk>\d+)/publish/', views.post_publish, name='post_publish'),
-
 ]
