d99e8419bf4f7d00ba4b5f9e1cbe7c4410c947bc
ma. fernanda rodriguez r <mafda13@gmail.com>
2020-05-03 13:05:44 -0300

    update Comment, indentation and fix bugs

--- blog/mysite/blog/forms.py
+++ blog/mysite/blog/forms.py
@@ -1,26 +1,26 @@
 from django import forms
-from blog.models import Post, Comments
+from blog.models import Post, Comment
 
 
 class PostForm(forms.ModelForm):
 
-	class Meta():
-		model = Post
-		fields = ('author', 'title', 'text')
+    class Meta():
+        model = Post
+        fields = ('author', 'title', 'text')
 
-		widgets = {
-			'title':forms.TextInput(attrs={'class':'textinputclass'}),
-			'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'})
-		}
+        widgets = {
+            'title':forms.TextInput(attrs={'class':'textinputclass'}),
+            'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'})
+        }
 
 
 class CommentForm(forms.ModelForm):
 
-	class Meta():
-		model = Comments
-		fields = ('author', 'text')
+    class Meta():
+        model = Comment
+        fields = ('author', 'text')
 
-		widgets = {
-			'author':forms.TextInput(attrs={'class':'textinputclass'}),
-			'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea'})
-		}
+        widgets = {
+            'author':forms.TextInput(attrs={'class':'textinputclass'}),
+            'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea'})
+        }
