4f62fe837054579c7fb47835769795277d348383
ma. fernanda rodriguez r <mafda13@gmail.com>
2020-05-03 13:01:55 -0300

    update Comment without s

--- blog/mysite/blog/admin.py
+++ blog/mysite/blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from blog.models import Post, Comments
+from blog.models import Post, Comment
 
 admin.site.register(Post)
-admin.site.register(Comments)
+admin.site.register(Comment)
